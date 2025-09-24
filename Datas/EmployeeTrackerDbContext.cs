0d798e34f2acb3862ebe72e6b040dd5b187f4528
Kavinasri-1043 <kavinasriganesh1043@gmail.com>
2025-09-24 13:22:21 +0530

    dbcontext created

---
+++ Datas/EmployeeTrackerDbContext.cs
@@ -0,0 +1,21 @@
+﻿using EmployeeTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeTracker.Datas
+{
+    public class EmployeeTrackerDbContext : DbContext
+    {
+        public EmployeeTrackerDbContext(DbContextOptions<EmployeeTrackerDbContext> options)
+        : base(options) { }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<WorkSession> WorkSessions { get; set; }
+        public DbSet<Break> Breaks { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
+        public DbSet<EmpTask> Tasks { get; set; }
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+
+
+
+    }
+}
