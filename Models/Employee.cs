a69c964cede6c71eccad1beac984da63a2bbce50
MADHANKUMAR1610 <madhanravirm@gmail.com>
2025-09-24 13:24:09 +0530

    error

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -20,7 +20,7 @@ namespace EmployeeTracker.Models
         public ICollection<WorkSession>? WorkSessions {  get; set; }
         public ICollection<Attendance>? Attendances{ get; set; }
         public ICollection<LeaveRequest>? Employees { get; set; }
-        public ICollection<EmpTask>? Tasks { get; set; }
+        public ICollection<Task>? Tasks { get; set; }
 
         
     }
