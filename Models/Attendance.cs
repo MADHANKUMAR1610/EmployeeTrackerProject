458786338e25d1ce706d9bcf4ea0d8b7b92906de
Kavinasri-1043 <kavinasriganesh1043@gmail.com>
2025-09-24 12:49:42 +0530

    Attendance, Leaverequest and task models created

---
+++ Models/Attendance.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeTracker.Models
+{
+    public class Attendance
+    {
+        [Key]
+        public int AttendanceId { get; set; }
+        [Required]
+        public int EmployeeId { get; set; }
+        [Required]
+        public Employee Employee { get; set; } 
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        public string Status { get; set; } = "Present";
+
+    }
+}
---
+++ Models/LeaveRequest.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeTracker.Models
+{
+    public class LeaveRequest
+    {
+        [Key]
+        public int LeaveRequestId { get; set; }
+        [Required]
+        public int EmployeeId { get; set; }
+        [Required]
+        public Employee Employee { get; set; }
+        [Required]
+        public DateTime StartTime { get; set; }
+        [Required]
+        public DateTime EndTime { get; set; }
+        
+        public string Reason { get; set; } = string.Empty;
+        
+        public string Status { get; set; } = "Pending";
+    }
+}
