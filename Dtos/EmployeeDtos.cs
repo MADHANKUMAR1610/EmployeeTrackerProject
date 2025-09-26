e3fd27fd64b64da40d1f929d0539e175d48c06f8
Kavinasri-1043 <kavinasriganesh1043@gmail.com>
2025-09-26 15:26:38 +0530

    kmm

---
+++ Dtos/EmployeeDtos.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeTracker.Dtos
+{
+    public class EmployeeDtos
+    {
+        public class EmployeeCreateDto
+        {
+            [Key]
+            public int EmployeeId { get; set; }
+            public string EmployeeName { get; set; } = string.Empty;
+            [EmailAddress]
+            public string Mail { get; set; } = string.Empty;
+            public string? Role { get; set; } 
+            public string ProfilePictureUrl { get; set; } = string.Empty;
+        }
+
+        // For Updating an existing Employee
+        public class EmployeeUpdateDto
+        {
+            public string EmployeeName { get; set; } = string.Empty;
+            public string? Role { get; set; } 
+            public string ProfilePictureUrl { get; set; } = string.Empty;
+        }
+
+        // For Reading/Returning Employee Data
+        public class EmployeeReadDto
+        {
+            public int EmployeeId { get; set; }
+            public string EmployeeName { get; set; } = string.Empty;
+            public string Mail { get; set; } = string.Empty;
+            public string Role { get; set; } = string.Empty;
+            public string ProfilePictureUrl { get; set; } = string.Empty;
+        }
+        public class EmployeeDeleteDto
+        {
+            
+            public int EmployeeId { get; set; }
+        }
+
+    }
+}
