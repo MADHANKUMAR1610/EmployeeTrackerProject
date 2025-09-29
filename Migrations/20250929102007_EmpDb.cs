9f012431e13121a97536c19602e6e503946c54fe
Kavinasri-1043 <kavinasriganesh1043@gmail.com>
2025-09-29 15:57:06 +0530

    final

--- Migrations/20250929102007_EmpDb.cs
+++ Migrations/20250929102007_EmpDb.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace EmployeeTracker.Migrations
 {
     /// <inheritdoc />
-    public partial class Updatedb : Migration
+    public partial class EmpDb : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
